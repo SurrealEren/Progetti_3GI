7273242ab64b4406261c73158591d3c48845f71b
SurrealEren <eashancoolest@gmail.com>
2024-05-08 12:46:55 +0200

    cambi

--- Primo_Progetto/Funzioni/FunzioniMain.py
+++ Primo_Progetto/Funzioni/FunzioniMain.py
@@ -51,14 +51,43 @@ def Orario_Docente(docenti,nomedocente):
 
 def Ore_Disponibili(docenti,nomedocente):
     orario=[]
+    
     for i in range(len(docenti)):
         if nomedocente in docenti[i]:
             for j in range(1,len(docenti[i])):
                 orario.append(docenti[i][j])
+    
     cont=0
     
     for i in range(len(orario)):
-        if orario[i]!=" D ":
+        if orario[i] == " D ":
             cont+=1
     
     return (cont)
+
+########################################################################
+
+def Docente_Ora_Specifica(docenti ,giorno, ora):
+    ListaDocenti=[]
+    "Prima variabile per calcolare dove dovrò puntare dopo"
+    p1=0
+    
+    if giorno=="LUN":
+        p1=0
+    elif giorno=="MAR":
+        p1=7
+    elif giorno=="MER":
+        p1=7*2
+    elif giorno=="GIO":
+        p1=7*3
+    elif giorno=="VEN":
+        p1=7*4
+        
+    punt = p1 + ora
+    
+    for i in docenti:
+        if len(i)>1:
+            if (i[punt]!="R" and i[punt]!="   "):
+                ListaDocenti.append(i[0])
+    
+    return(ListaDocenti)
--- Primo_Progetto/Main.py
+++ Primo_Progetto/Main.py
@@ -19,10 +19,12 @@ def main():
     orario_docenti=f.Docenti(righe_file)
     
     scelta=menù()
+    
     if scelta==1:
         classe=Funzioniinput.inputstr("Inserire la classe di cui si vogliono sapere i docenti: ").upper()
         elenco=f.Elenco_Doc_Classe(classe, orario_docenti)
         filenuovo=open("Docenti della {}.txt".format(classe), "w")
+        
         for i in range(len(elenco)):
             filenuovo.write("-" + elenco[i] + "\n")
     elif scelta==2:
@@ -33,6 +35,7 @@ def main():
         primerighe=righe_file[0] + righe_file[1]
         filenuovo.write(primerighe)
         filenuovo.write("\n" + docente20 + ",")
+        
         for i in range(len(orario)):
             if i!=len(orario):
                 filenuovo.write(orario[i] + ",")
@@ -40,10 +43,19 @@ def main():
                 filenuovo.write(orario[i])
         filenuovo.write("\nLe numero di ore buche del docente è: " + str(ore_buche))
     elif scelta==3:
-        docente=Funzioniinput.inputstr("Inserire il nome del docente di cui si vuole sapere l'orario: ").upper()
+        docente=Funzioniinput.inputstr("Inserire il nome del docente di cui si vuole sapere le ore disponibili: ").upper()
         docente20="{:20}".format(docente)
-        disponibilita=Ore_Disponibili(orario_docenti, docente20)
-        print()
+        disponibilita=f.Ore_Disponibili(orario_docenti, docente20)
+        filenuovo=open("Disponibilità di {}.txt".format(docente), "w")
+        filenuovo.write("Le ore disponibili di {0}, sono: {1}".format(docente, disponibilita))
+    elif scelta==4:
+        giorno=Funzioniinput.inputstr("Inserire le prime tre lettere del giorno: ").upper()
+        ora=Funzioniinput.inputint("Inserire l'ora come numero: ")
+        ListaDocenti=f.Docente_Ora_Specifica(orario_docenti, giorno, ora)
+        filenuovo=open("Lista dei docenti.txt", "w")
+        filenuovo.write("Lista dei docenti disponibili il {0} alla {1}° ora:\n".format(giorno,ora))
+        for i in ListaDocenti:
+            filenuovo.write("   -" + i + "\n")
     return
 
 ########################################################################
