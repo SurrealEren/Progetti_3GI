363a46f1f26e0fdff36d60e52bf6f8cc1eedaace
SurrealEren <eashancoolest@gmail.com>
2024-05-08 11:23:07 +0200

    Update FunzioniMain.py

--- Primo_Progetto/Funzioni/FunzioniMain.py
+++ Primo_Progetto/Funzioni/FunzioniMain.py
@@ -1,4 +1,3 @@
-import Funzioniinput
 import os
 
 ########################################################################
@@ -49,3 +48,17 @@ def Orario_Docente(docenti,nomedocente):
     return (orario,cont)
 
 ########################################################################
+
+def Ore_Disponibili(docenti,nomedocente):
+    orario=[]
+    for i in range(len(docenti)):
+        if nomedocente in docenti[i]:
+            for j in range(1,len(docenti[i])):
+                orario.append(docenti[i][j])
+    cont=0
+    
+    for i in range(len(orario)):
+        if orario[i]!=" D ":
+            cont+=1
+    
+    return (cont)
